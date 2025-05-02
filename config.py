36b555b87876528200ed66cee6c9c025f1b6c8ed
armanmd85000 <154997050+armanmd85000@users.noreply.github.com>
2025-05-02 06:01:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "22467463"
 api_hash = "156c0c4ecf6c03b4eb4ea9ac36e465d0"
 bot_token = "7452916609:AAEqzdgr9Rq623HdUKa8g9IUnjuPp56q5xk"
-auth_users = [1621539522,6839270382]
-sudo_users = [1621539522,6839270382]
+auth_users = [1621539522]
+sudo_users = [1621539522]
